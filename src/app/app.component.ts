123fb2a54926aeb240aa9b01e3b222b71d67a5fb
marco-bertelli <marcobert37@gmail.com>
2020-03-23 10:14:12 +0100

    commponent base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PrimaEs';
+  title = 'proviamo';
+  lanciato=0;
+  valore="";
+
+
+  lista = ['home', 'lista'];
+
+  elimina(indice:number) {
+    this.title="";
+    this.lanciato=1;
+  }
+  visto(){
+    if(this.lanciato==1)return true;
+    else return false;
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { GameListComponent } from './game-list/game-list.component';
+import { GameDetailComponent } from './game-detail/game-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    GameListComponent,
+    GameDetailComponent
   ],
   imports: [
     BrowserModule
