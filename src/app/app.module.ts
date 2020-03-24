1fbe96c6538dab2e835345b2a0c3ffe588294dfd
marco-bertelli <marcobert37@gmail.com>
2020-03-24 11:15:14 +0100

    gestione page-not-found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { GameListComponent } from './game-list/game-list.component';
 import { GameDetailComponent } from './game-detail/game-detail.component';
 import { RoutingGame } from './app-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RoutingGame } from './app-routing.module';
     MenuComponent,
     HomeComponent,
     GameListComponent,
-    GameDetailComponent
+    GameDetailComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
