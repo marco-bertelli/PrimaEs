8191c3bd5c970ca2dacfafadb2476b50f7f0268b
marco-bertelli <marcobert37@gmail.com>
2020-03-30 09:51:06 +0200

    page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GameListComponent } from './game-list/game-list.component';
 import { GameDetailComponent } from './game-detail/game-detail.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 
-
-const appRoutes: Routes = [
+const appRoutes: Routes = [ //cambia questa
     { path: 'home', component: HomeComponent },
     { path: 'game-list', component: GameListComponent},
     { path: 'game-detail', component: GameDetailComponent},
     { path: '', redirectTo: '/home', pathMatch: 'full' },
-   // { path: '**', component: PageNotFoundComponent }
+    { path: '**', component: PageNotFoundComponent }
 ];
 
 @core.NgModule({
